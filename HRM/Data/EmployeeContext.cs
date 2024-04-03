c0de5026fec7d1c240b80ac8a13844c02634ca21
nguyentai1010 <111493850+nguyentai1010@users.noreply.github.com>
2024-04-03 15:49:57 +0700

    g
    g

---
+++ HRM/Data/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using HRM.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRM.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; } = null!;
+    }
+}
+
